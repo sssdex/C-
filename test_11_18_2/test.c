92f6ec28ecec54f46bafbfb0e538c9d58294c125
记忆cell <3508084803@qq.com>
2023-11-18 12:10:26 +0800

    C语言操作符练习

---
+++ test_11_18_2/test.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<math.h>
+//int main()
+//{
+//	const double pi = acos(-1.0);
+//	double r, h, s1, s2, s;
+//	r = 1;
+//	h = 2;
+//	s1 = pi*r*r;
+//	s2 = 2*pi*r*h;
+//	s = s1*2.0 + s2;
+//	printf("Area = %.3f\n",s);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	printf("%d%d%d",n%10,n/10%10,n/100%10);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	int m = (n%10)*100+(n/10%10)*10+n/100;
+//	printf("%d",m);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d",&a,&b);
+//	a = a^b;
+//	b = a^b;
+//	a = a^b;
+//	printf("%d\n %d",a,b);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	size_t num = 0;
+//	int count = 0;
+//	scanf("%d",&num);
+//	while(num)
+//	{
+//		if(num%2 == 1)
+//			count++;
+//		num/=2;
+//	}
+//	printf("%d",count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d",&n);
+//	int count = 0;
+//	int i = 0;
+//	for( i = 0; i < 32; i++);
+//	{
+//		if(((n >> i)&1) == 1)
+//		  count++;
+//	}
+//	printf("%d",count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 13;
+//	printf("%d\n",n);
+//    n = n|(1<<4);
+//	printf("%d\n",n);
+//	n = n&~(1<<4);
+//	printf("%d\n",n);
+//	return 0;
+//}
