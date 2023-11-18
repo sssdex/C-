9e39394674be47595ff8c8d67b82947c65cbec84
����cell <3508084803@qq.com>
2023-11-18 15:26:05 +0800

    指针模拟二维数组
    Signed-off-by: ����cell <3508084803@qq.com>

---
+++ test_11_16/test.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5};
+//	int arr2[] = { 2,3,4,5,6};
+//	int arr3[] = { 3,4,5,6,7};
+//	int* arr[] = {arr1,arr2,arr3};
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < 5; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
+	int arr[][5] = {arr1[],arr2,arr3};
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
